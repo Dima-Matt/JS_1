6927b552ee947f6a90d3ebc9f15a3fc6b9755645
Dima Mironov <mattmars9548@gmail.com>
2024-02-29 21:00:21 +0300

    add files

---
+++ index.js
@@ -0,0 +1,10 @@
+let someText = 'How old are you?';
+let age = 100;
+let promptResult = prompt(someText, '');  // prompt возвращает строку или null
+console.log(promptResult);
+
+
+let isAge = confirm(someText);  // confirm вернёт true or false
+
+
+//let isBoolean = true; // is(has)
