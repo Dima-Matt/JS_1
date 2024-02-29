8007a4a6bd087dda1049ef0483b514c28542c683
Dima Mironov <mattmars9548@gmail.com>
2024-02-29 21:18:39 +0300

    df

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let someText = 'How old are you?';
-let age = 100;
-let promptResult = prompt(someText, '');  // prompt возвращает строку или null
-console.log(promptResult);
+//let age = 100;
+let promptResult = prompt(someText, '100');  // prompt возвращает строку или null
+//console.log(promptResult);
 
 
-let isAge = confirm(someText);  // confirm вернёт true or false
+//let isAge = confirm(someText);  // confirm вернёт true or false
 
 
 //let isBoolean = true; // is(has)
+console.log(`This man is ${promptResult}!`);
+console.log('jchhch ' + promptResult + ' jhjh hjjh');
